8d75691ef0db7866f46a3978ae5313f3ccde4e5e
The_Valour <108991640+TheValour@users.noreply.github.com>
2022-10-11 22:48:35 +0530

    Update inilazation.cpp

--- inilazation.cpp
+++ inilazation.cpp
@@ -15,23 +15,30 @@ int main(){
 	}
 
 	
-	// function initilaztion
+	// function-type initilaztion
 	{
-	// it will not give error	
-	int rage(20.99);
-	cout<<rage<<endl;
+		// it will not give error	
+		int rage(20.99);
+		cout<<rage<<endl;
 	}
 
-	// initilaztion string and char 
-	char ch{'a'};
-	string st{"hello"};
-	cout<<endl<<st;
-	cout<<endl<<ch;
 	
 	
 	{
+		// initilaztion string and char 
+		char ch{'a'};
+		cout<<endl<<ch;
+		char str=65;
+		cout<<endl<<str;        // it will print A
+		string st{"hello"};
+		cout<<endl<<st;
+	}
 	
-		// floating value
+	
+	
+	{
+		// floating value  by default it is double
+		
 		float f = 234.324f;               // size is  4 byte
 		double df = 2343256.345;          // size is 8 byte
 		long double ld{31446543.324L};    // size is 12 byte in my PC  but  in most time it is 16 byte
@@ -39,6 +46,16 @@ int main(){
 		cout<<endl<<f;
 		cout<<endl<<df;
 		cout<<endl<<ld;
+		
+		
+		//NAN  and infinity & -infinity 
+		
+		double a{23.23};
+		double na{-23.23};
+		double b{},c{};    // b will be initialised with 0
+		cout<<a/b<<endl;
+		cout<<na/b<<endl;
+		cout<<c/b<<endl;
 	}
 	
 	

6ffe5fcd9ec99e53ce22ce6696f8babaf8fd04f5
The_Valour <108991640+TheValour@users.noreply.github.com>
2022-10-11 19:45:08 +0530

    Create inilazation.cpp

---
+++ inilazation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	// brace initilaztion
+	{
+		// it give error of narrowing conversion 
+
+		int i{};     // initialized built-in type, equals to int i{0};
+		int j{10}; // initialized built-in type
+		
+		int age{29.99};
+		cout<<age<<endl;
+	}
+
+	
+	// function initilaztion
+	{
+	// it will not give error	
+	int rage(20.99);
+	cout<<rage<<endl;
+	}
+
+
+	int a=33.44;
+	cout<<a;
+
+
+}
